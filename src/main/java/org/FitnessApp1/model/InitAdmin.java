d8ae3f6a03fa8e7b65fc5140634552565ebd3b02
Mustafa Al-Saffar <mustafa.alsaffar@hotmail.com>
2025-06-07 20:29:37 +0200

    Remove unused methodes

--- src/main/java/org/FitnessApp1/model/InitAdmin.java
+++
@@ -1,9 +0,0 @@
-package org.FitnessApp1.model;
-
-public class InitAdmin {
-    public static void main(String[] args) {
-        AdminDAO dao = new AdminDAO();
-        boolean skapad = dao.createAdmin("123");
-        System.out.println("Admin skapad? " + skapad);
-    }
-}
